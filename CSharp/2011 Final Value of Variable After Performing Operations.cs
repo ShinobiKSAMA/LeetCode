96ad45a852efb9454ea6e6467febb55b8cf25163
ShinobiKSAMA <adityagmhatre@gmail.com>
2023-06-02 21:23:24 +0530

    Added 9 and 2011

--- CSharp/2011 Final Value of Variable After Performing Operations.cs
+++ CSharp/2011 Final Value of Variable After Performing Operations.cs
@@ -1,8 +1,25 @@
+public class Solution {
+    public int FinalValueAfterOperations(string[] operations) {
+        //Initialize Summation variable.
+        int finalValue = 0;
+
+        //Traverse through the operations list.
+        foreach (string x in operations) {
+            //If current operations contains '+', increment finalValue by 1. Else do the decrement by 1.
+            finalValue += x.Contains("+") ? 1 : -1;
+        }
+
+        //After traversing is completed, return latest value of summation variable
+        return finalValue;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
         Solution sol = new Solution();
-        Console.WriteLine(sol.IntToRoman(743));
+        string[] operations = new string[] {"X++", "++X", "X--"};
+        Console.WriteLine(sol.FinalValueAfterOperations(operations));
     }
 }
--- CSharp/9 Palindrome Number.cs
+++ CSharp/9 Palindrome Number.cs
@@ -1,8 +1,31 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        //Integer variable to create reverse of the number.
+        int rev = 0;
+
+        //Temp variable to calculate reverse.
+        int tmp = x;
+
+        //Until tmp becomes 0, keep destroying the tmp.
+        while (tmp > 0) {
+            //Store new value after destruction
+            int digit = tmp % 10;
+            //Add that digit to already stored rev.
+            rev = rev * 10 + digit;
+            //And get after remainder.
+            tmp /= 10;
+        }
+
+        //If rev and x are still same, it is Palindrome
+        return rev == x;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
         Solution sol = new Solution();
-        Console.WriteLine(sol.IntToRoman(743));
+        Console.WriteLine(sol.IsPalindrome(547));
     }
 }
